114402343f346a301cc8c03317ecc4c0012be22f
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-24 13:12:43 +0200

    cambios realizados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Entornos de desarrolo: Tarea 8");
+	System.out.println("Estos cambios los ha realizado Eliel Jesús Garcia Vela");
     }
 }
